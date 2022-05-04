024e154d84279874024af7dee8f3f8415fb64819
dx2r <gert.vanhollebeke@gmail.com>
2022-05-04 17:27:50 +0200

    Update TF_Average_Relative_Power_All_Frequencybands.m
    small updates

--- Time-Frequency Analyses/TF_Average_Relative_Power_All_Frequencybands.m
+++ Time-Frequency Analyses/TF_Average_Relative_Power_All_Frequencybands.m
@@ -2,7 +2,7 @@ function [power_values] = TF_Average_Relative_Power_All_Frequencybands(data, sam
 
 %%%
 % Function which calculates the average relative power of the timeseries in the data object. Total frequency range is defined as the lower limit of the delta range and the upper limit of the beta range.
-% NOTE: This function uses the "TF_Relative_Power" function for the individual epoch calculations
+% NOTE: This function uses the "TF_Relative_Power_All" function for the individual epoch calculations
 %
 % INPUT:
 %   data: N*M double matrix. (N = #timeseries, M=  #timepoints
@@ -18,7 +18,7 @@ function [power_values] = TF_Average_Relative_Power_All_Frequencybands(data, sam
 %   power_values = 4*N double indicating the relative power value of each frequency band, for each time series 
 %
 % EXAMPLE: 
-%   [power_values] = TF_Relative_Power(data, 512, 0.5, [0.5 4], [4 8], [8 13], [13 30]);
+%   [power_values] = TF_Relative_Power(data, 512, 3, 0.5, [0.5 4], [4 8], [8 13], [13 30]);
 %
 % Gert Vanhollebeke (24/01/2022)
 %
@@ -34,7 +34,7 @@ disp(strcat("Current participant: ", num2str(epoch_amount), " epochs..."));
 for current_epoch = 1:epoch_amount
     disp(strcat("Epoch ", num2str(current_epoch),"..."));
     current_data = data(:,(current_epoch - 1) + 1:(current_epoch - 1) + (epoch_length*sample_freq));
-    current_powers = TF_Relative_Power(current_data, sample_freq, bin_width, delta_range, theta_range, alpha_range, beta_range);    
+    current_powers = TF_Relative_Power_All(current_data, sample_freq, bin_width, delta_range, theta_range, alpha_range, beta_range);    
     power_values = power_values + current_powers;
 end
 %Step 3: divide the relative powers by the amount of epochs
