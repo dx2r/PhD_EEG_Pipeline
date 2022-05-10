af2af999f5b0091b9c57c7a5d5eea74fc3734797
dx2r <gert.vanhollebeke@gmail.com>
2022-05-10 17:20:51 +0200

    Update Build_Statistical_Table.m
    updated to also work with FC measures

--- Helper Functions/Build_Statistical_Table.m
+++ Helper Functions/Build_Statistical_Table.m
@@ -54,5 +54,37 @@ if(strcmp(analysis_type,"average_relative_power_specific"))
     statistical_table.Properties.VariableNames = column_names; %add names to the table          
 end
 
+%%%
+% AMPLITUDE ENVELOPE CORRELATION
+%%%
+
+if(strcmp(analysis_type,"amplitude_envelope_correlation"))
+    %tell what is going on
+    disp("Analysis Type: Amplitude Envelope Correlation...");
+    %build an empty array for all results
+    result_table = zeros(size(path_to_result_files,1), (brainregion_amount*(brainregion_amount-1))/2 );
+    %build empty cell array for the participant names
+    participant_names = cell([size(path_to_result_files,1),1]);
+    %build the variablenames cell array for the final table
+    column_names = cell(1,1 + (brainregion_amount*(brainregion_amount-1))/2);
+    column_names(1,1) = cellstr("Participant_Name");
+    column_names(1,2:end) = Convert_Brainregion_Names_To_FC_Compatible(brainregion_names,"undirectional");
+    %loop over all results
+    for result_i = 1:size(path_to_result_files,1)
+        %get current result
+        current_result = Extract_Object_From_Structure(path_to_result_files(result_i));
+        %extract name from path file
+        current_name = extractAfter(path_to_result_files(result_i),result_map);
+        current_name = extractBetween(current_name,"\",".mat");
+        %put name and result in corresponding array
+        participant_names(result_i,1) = cellstr(current_name);
+        result_table(result_i,:) = Convert_Undirected_FC_Matrix_To_Line(current_result);
+    end
+    %built final table
+    result_table = num2cell(result_table); %double array to cell array
+    statistical_table = [participant_names, result_table]; %combine both cell arrays
+    statistical_table = cell2table(statistical_table); %convert cell array to table
+    statistical_table.Properties.VariableNames = column_names; %add names to the table          
+end
 
 end
