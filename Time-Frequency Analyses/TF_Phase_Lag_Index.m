47f0c71eda9ed26a12c11abd6957f553cb68ad32
dx2r <gert.vanhollebeke@gmail.com>
2022-06-14 12:04:05 +0200

    Update TF_Phase_Lag_Index.m
    Phase Lag Function, completed

--- Time-Frequency Analyses/TF_Phase_Lag_Index.m
+++ Time-Frequency Analyses/TF_Phase_Lag_Index.m
@@ -14,15 +14,37 @@ function [PLI_values] = TF_Phase_Lag_Index(data, epoch_length, sample_freq, freq
 % OUTPUT:
 %   PLI_Values: NxN double array, containing the PLI values.
 %
+% EXAMPLE: 
+%   PLI_Values = TF_Phase_Lag_Index(data, 3, 512, [8 13]);
+%
 %%%
 
 %Step 0: tell what is going on
 disp("Computing Phase Lag Index...");
-
 %Step 1: compute PLI
+[timeseries_amount, timeseries_length] = size(data); %get info of data
 data = data.'; %transpose data, works easier in following steps
 data_bandpass = bandpass(data,freq_range, sample_freq); %bandpass data
 data_hilbert = hilbert(data_bandpass); %compute hilbert transform
+data_phase = angle(data_hilbert); %compute phase of singal from hilbert transform
+%compute PLI for each combination of signals
+PLI_values = zeros(timeseries_amount, timeseries_amount); %create empty matrix for PLI values storage
+for loop_1 = 1:timeseries_amount-1
+    for loop_2 = loop_1 + 1:timeseries_amount
+        %get the two currently needed timeseries
+        signal_1 = data_phase(:,loop_1);
+        signal_2 = data_phase(:,loop_2);
+        %obtain difference signal
+        signal_difference = signal_1 - signal_2;
+        %obtain sign function results from difference signal
+        signal_sign = sign(signal_difference); 
+        %obtain PLI value
+        current_PLI = abs(mean(signal_sign));
+        %store PLI value (twice, PLI is undirected)
+        PLI_values(loop_1, loop_2) = current_PLI;
+        PLI_values(loop_2, loop_1) = current_PLI;
+    end
+end
 
 
 
