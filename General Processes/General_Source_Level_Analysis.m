eb8ee44dc717616d3bcdc4a09e617939552ed158
dx2r <gert.vanhollebeke@gmail.com>
2022-09-15 14:50:37 +0200

    Update General_Source_Level_Analysis.m
    updated commenting

--- General Processes/General_Source_Level_Analysis.m
+++ General Processes/General_Source_Level_Analysis.m
@@ -122,6 +122,8 @@ clear all; %clear workspace
         %   "average_relative_power_all" -> get average relative power from the delta, theta, alpha and beta frequency range  
         %   "average_relative_power_specific" -> get average relative power from specific frequency range
         %   "average_absolute_power_specific" -> get average absolute power from specific frequency range
+        %   "average_log_absolute_power_specific" -> get log transformed version of average absolute power from specific frequency range
+        %   "average_relative_power_specific_fcorrected" -> get relative power for specific frequency range, with 1/f correction
         
         analysis_choice_power = "";  
         
@@ -132,6 +134,10 @@ clear all; %clear workspace
         %   pow_varargin = {0.5, [8 13], [0.5 40]};
         % for "average_absolute_power_specific": {bin_width, frequency_range_of_interest, whole_frequency_range}
         %   pow_varargin = {0.5, [8 13], [0.5 40]};
+        % for "average_log_absolute_power_specific": {bin_width, frequency_range_of_interest, whole_frequency_range}
+        %   pow_varargin = {0.5, [8 13], [1 40]};
+        % for "average_relative_power_specific_fcorrected": {bin_width, frequency_range_of_interest, whole_frequency_range, gamma}
+        %   pow_varargin = {0.5, [8 13], [1 40], 1};
 
         pow_varargin = {};
         
