f9c7fbd06c11f1ffdfd5ae4b53f55aaa3c497f22
loreflipts <112189990+loreflipts@users.noreply.github.com>
2023-03-09 15:13:22 +0100

    Create Calculate_Spatial_Correlation.m

---
+++ Microstates Analyses/Calculate_Spatial_Correlation.m
@@ -0,0 +1,20 @@
+function [spatial_correlation] = Calculate_Spatial_Correlation(potential_map_u, potential_map_v)
+
+%%%
+% [1] M. M. Micah, M. De Lucia, D. Brunet, and C. M. Michel, ‘Principles of Topographic Analyses for Electrical Neuroimaging’, 
+% in Brain Signal Analysis: Advances in Neuroelectric and Neuromagnetic Methods, 
+% T. C. Handy, Ed. The MIT Press, 2009, p. 0. doi: 10.7551/mitpress/9780262013086.003.0002.
+%
+% Function that ...
+% 
+% Lore Flipts
+%
+% INPUT:
+%   
+%
+% OUTPUT:
+%   
+%
+%%%
+
+end
