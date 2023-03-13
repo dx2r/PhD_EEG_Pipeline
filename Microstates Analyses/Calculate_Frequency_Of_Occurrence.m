2a6fb944c218b5685e88de3cc5624d6a4224b15c
loreflipts <112189990+loreflipts@users.noreply.github.com>
2023-03-13 13:38:23 +0100

    Create Calculate_Frequency_Of_Occurrence.m

---
+++ Microstates Analyses/Calculate_Frequency_Of_Occurrence.m
@@ -0,0 +1,25 @@
+function [freq_of_occurrence] = Calculate_Frequency_Of_Occurrence(data, labels, microstates, sample_freq)
+
+%%%
+% Function that ...
+% 
+% Lore Flipts
+%
+% INPUT:
+%   
+%
+% OUTPUT:
+%   
+%
+%%%
+K = size(microstates,2);
+freq_of_occurrence = zeros(1,K);
+[~, timepoints_amount] = size(data);
+seconds_amount = timepoints_amount ./ sample_freq;
+
+for k = 1:K
+
+    freq_of_occurrence(k) = sum((labels == k)) ./ seconds_amount;
+end
+
+end
