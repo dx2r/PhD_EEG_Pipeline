b6cbf005655f58df89b19865ec41a0ab66265ea0
loreflipts <112189990+loreflipts@users.noreply.github.com>
2023-03-20 17:17:26 +0100

    Create Calculate_Transition_Probabilities.m

---
+++ Microstates Analyses/Calculate_Transition_Probabilities.m
@@ -0,0 +1,28 @@
+function [transition_probabilities] = Calculate_Transition_Probabilities(data, labels, microstates)
+
+%%%
+% Function that ...
+% 
+% Lore Flipts
+%
+% INPUT:
+%   
+%
+% OUTPUT:
+%   
+%
+%%%
+microstates_amount = size(microstates,2);
+transition_probabilities = zeros(microstates_amount);
+%[~, timepoints_amount] = size(data);
+labels_k = labels(1:end-1);
+labels_j = labels(2:end);
+for microstate_k = 1:microstates_amount
+    for microstate_j = 1:microstates_amount
+        transition_probabilities(microstate_k, microstate_j) = sum((labels_k == microstate_k) & (labels_j == microstate_j));
+    end
+end
+
+transition_probabilities = transition_probabilities ./ sum(transition_probabilities,2);
+
+end
